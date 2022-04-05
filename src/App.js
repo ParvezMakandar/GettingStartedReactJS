e8772c14426f4b5bb81edcba2fcb16ddce435b19
parvez050 <parvezhm050@gmail.com>
2022-04-06 03:49:54 +0530

    user input name

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ class App extends React.Component{
     this.state = {
       title: 'Hello From Bridgelabz'
     }
+    this.state = {
+       userName: '' 
+    }
   }
 
   // OnClick function
@@ -17,14 +20,26 @@ class App extends React.Component{
     console.log("save button is clicked", $event);
     window.open(this.url, "_blank");
   }
+
+  onNameChange = (event) => {
+    console.log("the value is", event.target.value);
+    this.setState( {userName: event.target.value})    
+  }
+
   render(){
     return(
       <div>
+      <div>
         <h1> {this.state.title}</h1>
+        <h1> Hello {this.state.userName} from Bridgelabz</h1>
         <img src={logo} onClick={this.onClick}
         alt="The Bridgelabz logo"/>
       </div>
-    )
+      <div>
+        <input onChange={this.onNameChange} />
+      </div>
+      </div>
+    );
   }
 }
 
