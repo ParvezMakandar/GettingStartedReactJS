05e727a40aa604fe9924dd7345d5f37e79520c5b
parvez050 <parvezhm050@gmail.com>
2022-04-06 02:46:45 +0530

    launching BridgeLabz Site in a new Tab on clicking BridgeLabz Logo

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import React from 'react';
 
 
 class App extends React.Component{
+  url = 'https://www.bridgelabz.com/'
   constructor(){
     super()
     this.state = {
       title: 'Hello From Bridgelabz'
     }
   }
+
+  // OnClick function
+  onClick = ($event) =>{
+    console.log("save button is clicked", $event);
+    window.open(this.url, "_blank");
+  }
   render(){
     return(
       <div>
         <h1> {this.state.title}</h1>
-        <img src={logo} 
+        <img src={logo} onClick={this.onClick}
         alt="The Bridgelabz logo"/>
       </div>
     )
